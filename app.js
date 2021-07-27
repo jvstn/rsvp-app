0ee8368acfde46d1e6d1bf5274af78e2f6962123
jmcfadd <justinmcfadd4@gmail.com>
2021-07-27 12:06:53 -0400

    Confirmed Styling

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('hello world from app.js')
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+  const ul = document.getElementById("invitedList");
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  const li = document.createElement('li');
+  li.textContent = text;
+  const label = document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
+  ul.appendChild(li);
+})
+
+ul.addEventListener('change', (e) => {
+  const checkbox = e.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+
+  if (checked) {
+    listItem.className = 'responded';
+  } else {
+    listItem.className = "";
+  }
+})
